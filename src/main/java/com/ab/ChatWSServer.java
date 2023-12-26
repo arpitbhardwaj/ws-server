0ff20bc3288578e6b7f461b150c13fc19932bf50
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2023-12-26 14:47:10 +0530

    Fixes

--- src/main/java/com/ab/ChatWSServer.java
+++ src/main/java/com/ab/ChatWSServer.java
@@ -5,27 +5,48 @@ import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/chat-ws-server/{name}")
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/chat-ws-server/{username}")
 @ApplicationScoped
 public class ChatWSServer {
 
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
     @OnOpen
-    public void onOpen(Session session, @PathParam("name") String name) {
-        System.out.println("onOpen> " + name);
+    public void onOpen(Session session, @PathParam("username") String name) {
+        System.out.println("onOpen > " + name);
+        sessions.put(name, session);
+        broadcast("User " + name + " joined");
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("name") String name) {
-        System.out.println("onClose> " + name);
+    public void onClose(Session session, @PathParam("username") String name) {
+        System.out.println("onClose > " + name);
+        sessions.remove(name);
+        broadcast("User " + name + " left");
     }
 
     @OnError
-    public void onError(Session session, @PathParam("name") String name, Throwable throwable) {
-        System.out.println("onError> " + name + ": " + throwable);
+    public void onError(Session session, @PathParam("username") String name, Throwable throwable) {
+        System.out.println("onError > " + name + ": " + throwable);
+        sessions.remove(name);
+        broadcast("User " + name + " left on error: " + throwable);
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("name") String name) {
-        System.out.println("onMessage> " + name + ": " + message);
+    public void onMessage(String message, @PathParam("username") String name) {
+        System.out.println("onMessage > " + name + ": " + message);
+        broadcast(">> " + name + ": " + message);
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result ->  {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
     }
 }
